54501d48f67f873ff014ca0a36e58ca9b2641b7e
Tatyanochka <tanyasmit16@yandex.ru>
2024-05-05 20:43:34 +0300

    succed build

--- src/test/java/OrderCardTest.java
+++ src/test/java/OrderCardTest.java
@@ -11,11 +11,11 @@ public class OrderCardTest {
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
+        SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
-        form.$("[data-test-id=submit]").click();
+        form.$(".button__text").click();
         $(".Success_successBlock__2L3Cw").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
